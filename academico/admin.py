412880f9d6dec615c835c56a770b401c373e515a
MiqCaceres <mdcaceres6@gmail.com>
2026-06-22 18:36:03 -0300

    fix/ui: corregir concordancia de genero en mensajes de exito del admin

--- academico/admin.py
+++ academico/admin.py
@@ -1,19 +1,36 @@
 from django.contrib import admin
-from .models import Facultad, Carrera, Materia  # <-- Asegúrate de importar Materia aquí
+from django.contrib import messages  # <-- 1. ASEGÚRATE DE IMPORTAR MESSAGES AQUÍ
+from .models import Facultad, Carrera, Materia
 
 @admin.register(Facultad)
 class FacultadAdmin(admin.ModelAdmin):
     list_display = ('id', 'nombre')
     search_fields = ('nombre',)
 
+    def message_user(self, request, message, level=messages.SUCCESS, extra_tags="", fail_silently=False):
+        if "El facultad" in str(message):
+            message = str(message).replace("El facultad", "La facultad").replace("fue agregado", "fue agregada")
+        super().message_user(request, message, level, extra_tags, fail_silently)
+
+
 @admin.register(Carrera)
 class CarreraAdmin(admin.ModelAdmin):
     list_display = ('id', 'nombre', 'facultad')
     list_filter = ('facultad',)
     search_fields = ('nombre',)
-    
-    
+
+    def message_user(self, request, message, level=messages.SUCCESS, extra_tags="", fail_silently=False):
+        if "El carrera" in str(message):
+            message = str(message).replace("El carrera", "La carrera").replace("fue agregado", "fue agregada")
+        super().message_user(request, message, level, extra_tags, fail_silently)
+
+
 @admin.register(Materia)
 class MateriaAdmin(admin.ModelAdmin):
     list_display = ('id', 'codigo', 'nombre')
     search_fields = ('codigo', 'nombre')
+    def message_user(self, request, message, level=messages.SUCCESS, extra_tags="", fail_silently=False):
+        if "El materia" in str(message):
+            message = str(message).replace("El materia", "La materia").replace("fue agregado", "fue agregada")
+        super().message_user(request, message, level, extra_tags, fail_silently)
+    
